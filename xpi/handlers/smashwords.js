1a7b6a8a8ff9aef6ed086435d2215d77013a8b2e
Dustin Brewer <mannkind@gmail.com>
2010-11-19 01:13:09 +0800

    Improved url/response url.

--- xpi/handlers/smashwords.js
+++ xpi/handlers/smashwords.js
@@ -2,12 +2,12 @@
 // Dustin Brewer <mannkind@gmail.com>
 register({
   name: 'Smashwords',
-  url: 'http://www.smashwords.com',
+  url: 'http://www.smashwords.com/profile',
   domains: [ 'smashwords.com' ],
   sessionCookieNames: [ 'sw', 'smashinflogin' ],
 
   identifyUser: function () { 
-    var resp = this.httpGet(this.siteUrl + '/profile');
+    var resp = this.httpGet(this.siteUrl);
     this.userName = resp.body.querySelector('div#pageContent h1').textContent.trim();
     this.userAvatar = resp.body.querySelector('div.leftSideBox div img').src;
   }
